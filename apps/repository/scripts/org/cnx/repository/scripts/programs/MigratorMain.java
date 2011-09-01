05ce2339007fa5f8402b44828bc91df4b5097c9c
Arjun Satyapal <arjuns@google.com>
2011-09-01 00:02:35 -0700

    1. Adding script to allow migration from command line. 2. Tests for migrators.

---
+++ apps/repository/scripts/org/cnx/repository/scripts/programs/MigratorMain.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright The CNX Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.cnx.repository.scripts.programs;
+
+import com.google.common.base.Preconditions;
+
+import com.sun.syndication.propono.atom.client.ClientEntry;
+
+import org.cnx.atompubclient.CnxAtomPubClient;
+import org.cnx.repository.scripts.migrators.ParallelCollectionMigrator;
+import org.cnx.repository.scripts.migrators.ParallelModuleMigrator;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+import java.net.URL;
+import java.util.logging.Logger;
+
+/**
+ * Migrator for migrating Modules to CNX repository.
+ * 
+ * TODO(arjuns) : Add java flags.
+ * 
+ * @author Arjun Satyapal
+ */
+public class MigratorMain {
+    private static Logger logger = Logger.getLogger(MigratorMain.class.getName());
+
+    private CnxAtomPubClient cnxClient;
+
+    @Option(name = "-migrateModule", usage = "do migration for module.")
+    private boolean migrateModule;
+
+    @Option(name = "-aerModuleId", usage = "ModuleId on AER.")
+    private String aerModuleId;
+
+    @Option(name = "-cnxModuleId", usage = "ModuleId on cnx.org.")
+    private String cnxModuleId;
+
+    @Option(name = "-migrateCollection", usage = "do migration for collection.")
+    private boolean migrateCollection;
+
+    @Option(name = "-aerCollectionId", usage = "CollectionId on AER.")
+    private String aerCollectionId;
+
+    @Option(name = "-cnxCollectionId", usage = "ModuleId on cnx.org.")
+    private String cnxCollectionId;
+
+    @Option(name = "-repositoryUrl", usage = "Repository URL")
+    private String atomPubServiceUrl = "http://localhost:8888/atompub";
+
+    @Option(name = "-localFolder", usage = "Location for module/collection")
+    private String localFolder = null;
+
+    @Option(name = "-preserveModuleIds", usage = "Preserve moduleIds for Collection.")
+    private boolean preserveModuleIds = false;
+
+    private final static String USAGE =
+        "\n\t-migrateModule -[aerModuleId | cnxModuleId] <id> -localFolder <location>"
+            + "\n\t-migrateCollection -[aerModuleId | cnxModuleId] <id> -localFolder <location> <-preserveModuleIds>";
+
+    public static void main(String[] args) throws Exception {
+
+        StringBuilder builder = new StringBuilder();
+        for (String currArg : args) {
+            builder.append(currArg).append(" ");
+        }
+
+        System.out.println("Args : " + builder.toString());
+
+        new MigratorMain().doMain(args);
+    }
+
+    public void doMain(String[] args) throws Exception {
+        long startTime = System.currentTimeMillis();
+
+        CmdLineParser parser = new CmdLineParser(this);
+
+        try {
+            parser.parseArgument(args);
+
+            StringBuilder builder = new StringBuilder();
+            builder.append("MigrateModule = ").append(migrateModule);
+            System.out.println(builder.toString());
+            if (migrateModule == migrateCollection) {
+                printErrAndExit("Usage = " + USAGE);
+            }
+
+            Preconditions.checkNotNull(localFolder, "localfolder is null");
+            Preconditions.checkNotNull(atomPubServiceUrl, "atomPubServiceUrl is null");
+
+            URL atomPubUrl = new URL(atomPubServiceUrl);
+            cnxClient = new CnxAtomPubClient(atomPubUrl);
+
+            if (migrateModule) {
+                migrateModule();
+            } else if (migrateCollection) {
+                migrateCollection();
+            }
+
+        } catch (CmdLineException e) {
+            printErrAndExit("Usage = " + USAGE);
+        }
+        long endTime = System.currentTimeMillis();
+        logger.info("Time to migrate = " + (endTime - startTime) / 1000);
+
+    }
+
+    private void migrateCollection() throws InterruptedException {
+        if (aerCollectionId != null) {
+            Preconditions.checkArgument(cnxCollectionId == null);
+        } else if (cnxCollectionId != null) {
+            Preconditions.checkArgument(aerCollectionId == null);
+        }
+
+        ParallelCollectionMigrator migrator =
+            new ParallelCollectionMigrator(cnxClient, localFolder, cnxCollectionId,
+                    aerCollectionId, null /* version */, preserveModuleIds);
+
+        Thread thread = new Thread(migrator);
+        thread.start();
+        thread.join();
+        // TODO(arjuns) : Check for exit status for thread.
+        ClientEntry newclientEntry = migrator.getCollectionVersionEntry();
+    }
+
+    public void printErrAndExit(String errmMsg) {
+        System.err.println(errmMsg);
+        System.exit(1);
+    }
+
+    private void migrateModule() throws InterruptedException {
+        if (aerModuleId != null) {
+            Preconditions.checkArgument(cnxModuleId == null);
+        } else if (cnxModuleId != null) {
+            Preconditions.checkArgument(aerModuleId == null);
+        }
+
+        ParallelModuleMigrator migrator =
+            new ParallelModuleMigrator(cnxClient, localFolder, null /* collXmlModuleId */,
+                    cnxModuleId, aerModuleId, null /* version */);
+
+        Thread thread = new Thread(migrator);
+        thread.start();
+        thread.join();
+        // TODO(arjuns) : Check for exit status for thread.
+        ClientEntry newclientEntry = migrator.getModuleVersionEntry();
+        long endTime = System.currentTimeMillis();
+    }
+
+    public void validateCollectionDetails() {
+        if (aerCollectionId != null) {
+            Preconditions.checkArgument(cnxCollectionId == null);
+        } else {
+            Preconditions.checkArgument(cnxCollectionId != null);
+        }
+    }
+}
